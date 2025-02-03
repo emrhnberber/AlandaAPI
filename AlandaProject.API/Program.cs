ac1b03244bcd95d5f6c439e9fa0853d36685a329
Emirhan Berber <emirhanberber2018@gmail.com>
2025-02-03 21:37:51 +0300

    Rate Limiter Update
    Added rate limiter for all controllers.

--- AlandaProject.API/Program.cs
+++ AlandaProject.API/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +27,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? String.Empty))
         };
     });
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "global",
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 10,  // Maksimum 10 istek
+                Window = TimeSpan.FromMinutes(1),  // Dakikada 10 istek
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0  // Kuyrukta beklenecek max istek bekleyebilir
+            }));
+
+    options.RejectionStatusCode = 429; // Too Many Requests (HTTP 429)
+});
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 builder.Services.AddScoped<TokenService>();
@@ -52,11 +70,9 @@ using (var scope = app.Services.CreateScope())
 //app.Urls.Add("http://0.0.0.0:5000"); // API'yi tüm IP'lerden eriþilebilir yapar
 //app.Urls.Add("http://localhost:5000");
 
-
-
+app.UseRateLimiter();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- AlandaProject.API/obj/Debug/net8.0/AlandaProject.API.AssemblyInfo.cs
+++ AlandaProject.API/obj/Debug/net8.0/AlandaProject.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlandaProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7c51778db33d097cce9a5bb4312e28ac22dd5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlandaProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlandaProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
